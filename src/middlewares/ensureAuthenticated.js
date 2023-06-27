4b532653f7d209a295b9eb8845cd51964082fad3
Wellinton Felipe <wellintonfelipe@hotmail.com.br>
2023-06-26 21:44:09 -0300

    debug, file ensureAuthenticated.js 🐛

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -2,17 +2,17 @@ import pkg from "jsonwebtoken";
 import { AppError } from "../utils/AppError.js";
 import { authConfig } from "../config/auth.js";
 
+const { verify } = pkg;
 export function ensureAuthenticated(request, response, next) {
   const authHeader = request.headers.authorization;
-
   if (!authHeader) {
-    throw new AppError("JWT Token invalido", 401);
+    throw new AppError("JWT Token não informado", 401);
   }
 
-  const [, token] = authHeader.split("");
+  const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, authHeader.split(""));
+    const { sub: user_id } = verify(token, authConfig.jwt.secret);
 
     request.user = {
       id: Number(user_id),
